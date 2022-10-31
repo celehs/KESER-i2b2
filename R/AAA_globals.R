fe2c371b8ba49233d5f9f174490f7234b4c25367
Cosmopolitan-Ou <jimou123456@outlook.com>
2022-10-31 22:00:18 +0800

    ram usage per code update

--- R/AAA_globals.R
+++ R/AAA_globals.R
@@ -3,4 +3,4 @@
 PKG.NAME <- "KESER.i2b2"
 DIM_MAX <- 2000                    # Maximum limit of embedding dimension
 ARP_CODI_IDX <- 1:9                # id range for codi items in all relation pairs file
-RAM_USAGE_PER_CODE <- 16 / 40000   # 40k unique codes use up 16gb of ram
+RAM_USAGE_PER_CODE <- 20 / 23000   # 23k unique codes use up 20gb of ram
